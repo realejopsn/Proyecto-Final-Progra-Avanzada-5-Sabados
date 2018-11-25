0709382ff7779b2a29367a9346f2744bae4aa6f6
realejopsn <alegonpov@gmail.com>
2018-11-25 12:10:53 -0600

    Creacion de la estructura basica del proyecto

---
+++ Fidelitas.Proyecto.ArticulosPerdidos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fidelitas.Proyecto.ArticulosPerdidos.MvcApplication" Language="C#" %>
---
+++ Fidelitas.Proyecto.ArticulosPerdidos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fidelitas.Proyecto.ArticulosPerdidos.Startup))]
+namespace Fidelitas.Proyecto.ArticulosPerdidos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
