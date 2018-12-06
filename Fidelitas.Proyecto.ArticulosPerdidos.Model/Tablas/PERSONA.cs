1ab4fa3ea2ca70e57ab86de0247aac8118d38376
Daniel Abraham Granados Infante <danieltec17@hotmail.com>
2018-12-06 17:21:56 -0600

    Entity creado, clases partial de las tablas y el conection strings en todos los poryectos

---
+++ Fidelitas.Proyecto.ArticulosPerdidos.Model/Tablas/PERSONA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fidelitas.Proyecto.ArticulosPerdidos.Model
+{
+  public partial  class PERSONA
+    {
+    }
+}
